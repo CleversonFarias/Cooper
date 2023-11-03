8f64681076de09576ca521515f07fad83fbb3bb3
Cleverson Farias <cleverson_sairaf@outlook.com>
2023-11-03 18:04:34 -0300

    init

---
+++ src/main/java/br/com/cleverson/cooper/CooperApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleverson.cooper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperApplication.class, args);
+	}
+
+}
